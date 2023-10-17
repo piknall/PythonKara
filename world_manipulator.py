0431e8941fbb10a38487c5abb45a7c988645c72e
= <pisafe@tutanota.com>
2023-10-17 13:21:22 +0200

    adding docstrings to ModificationHandler and its dependency classes

--- world_manipulator.py
+++ world_manipulator.py
@@ -42,6 +42,9 @@ class WorldManipulator:
 
 @dataclass
 class ColourModificationPoint:
+    """
+    class to document changes made to a worlds colour_layer
+    """
     position: tuple[int, int] | np.ndarray[int, int]
     size: tuple[int, int] | np.ndarray[int, int]
     colour_matrix: np.ndarray[np.ndarray[np.ndarray[int, ...], ...], ...]
@@ -49,6 +52,9 @@ class ColourModificationPoint:
 
 @dataclass
 class ObjectModificationPoint:
+    """
+    class to document changes made to a worlds colour_layer
+    """
     position: tuple[int, int] | np.ndarray[int, int]
     size: tuple[int, int] | np.ndarray[int, int]
     leaf_matrix: np.ndarray[np.ndarray[int, ...], ...]
@@ -57,10 +63,19 @@ class ObjectModificationPoint:
 
 class ModificationGroup:
     def __init__(self, reproducible: bool = True):
+        """
+        class to bundle multiple ModificationPoints together
+        :param reproducible: determines if the changes documented in the ModificationGroups ModificationPoints can be
+                             redone if undone before
+        """
         self.modification_points = []
         self.reproducible = reproducible
 
-    def append_point(self, modification_point: ColourModificationPoint | ObjectModificationPoint):
+    def append_point(self, modification_point: ColourModificationPoint | ObjectModificationPoint) -> None:
+        """
+        add ModificationPoint to ModificationGroup
+        :param modification_point: ModificationPoint to be added
+        """
         self.modification_points.append(modification_point)
 
 
@@ -69,6 +84,10 @@ class ModificationHandler:
     OBJECT_MODIFICATION = "object_modification"
 
     def __init__(self, world_object: world.World):
+        """
+        class to store parts of a World to enable undo and redo functionality
+        :param world_object: world to log changes of
+        """
         self.world = world_object
         self.undo_depth = 0
         self.undo_modification_groups = []
@@ -78,6 +97,14 @@ class ModificationHandler:
                                    position: tuple[int, int] | np.ndarray[int, int],
                                    size: tuple[int, int] | np.ndarray[int, int],
                                    modification_type: str) -> ColourModificationPoint | ObjectModificationPoint:
+        """
+        private method to create a Colour or ObjectModificationPoint of a given size at a given position of the world
+        :param position: left up corner of the area in the world to be stored
+        :param size: tuple of s and y size of the area in the world to be stored
+        :param modification_type: modification type konstant indicating if the worlds colour_layer (COLOUR_MODIFICATION)
+                                  or the worlds leaf or object_layer (OBJECT_MODIFICATION) is going to be modified
+        :return: ModicationPoint object
+        """
         if modification_type == self.COLOUR_MODIFICATION:
             return ColourModificationPoint(
                 position=position,
@@ -101,21 +128,37 @@ class ModificationHandler:
     def add_modification_point(self,
                                position: tuple[int, int] | np.ndarray[int, int],
                                size: tuple[int, int] | np.ndarray[int, int],
-                               modification_type: str):
+                               modification_type: str) -> None:
+        """
+        method to log the world's state by defining the position of the changes to be made soon
+        :param position: left up corner of the area in the world to be stored
+        :param size: tuple of s and y size of the area in the world to be stored
+        :param modification_type: modification type konstant indicating if the worlds colour_layer (COLOUR_MODIFICATION)
+                                  or the worlds leaf or object_layer (OBJECT_MODIFICATION) is going to be modified
+        """
         self.undo_modification_groups[-1].append_point(
             self._create_modification_point(position,
                                             size,
                                             modification_type)
         )
 
-    def add_modification_group(self, reproducible: bool = True):
+    def add_modification_group(self, reproducible: bool = True) -> None:
+        """
+        method to add a modification group, modification groups are used to bundle multiple modification points together
+        :param reproducible: if True once the created ModificationGroup is undone it can be redone else not
+        """
         self.undo_modification_groups.append(ModificationGroup(reproducible))
 
         self.redo_modification_groups = []
         self.redo_modification_groups = self.redo_modification_groups[:-1 - self.undo_depth]
         self.undo_depth = 0
 
-    def _create_undo_modification_group(self, modification_group: ModificationGroup) -> ModificationGroup:
+    def _create_redo_modification_group(self, modification_group: ModificationGroup) -> ModificationGroup:
+        """
+        creates a ModificationGroup based on the changes that are going to be made by another ModificationGroup soon
+        :param modification_group: ModificationGroup to be inverted
+        :return: ModificationGroup that can be used to undo the changes made by the specified ModificationGroup
+        """
         undo_modification_group = ModificationGroup()
 
         for modification_point in modification_group.modification_points:
@@ -131,7 +174,11 @@ class ModificationHandler:
 
         return undo_modification_group
 
-    def _apply_modification_group(self, modification_group: ModificationGroup):
+    def _apply_modification_group(self, modification_group: ModificationGroup) -> None:
+        """
+        restore the worlds status stored in the specified ModificationGroup
+        :param modification_group: ModificationGroup to restore from
+        """
         for modification_point in modification_group.modification_points:
             if isinstance(modification_point, ColourModificationPoint):
 
@@ -159,7 +206,10 @@ class ModificationHandler:
                 raise ValueError(f"modification point: '{modification_point}' in "
                                  f"modification group: '{modification_group}' is invalid")
 
-    def undo(self):
+    def undo(self) -> None:
+        """
+        method to undo the latest not yet undone ModificationGroup
+        """
         if self.undo_depth + 1 > len(self.undo_modification_groups):
             return None
 
@@ -167,7 +217,7 @@ class ModificationHandler:
 
         if targeted_modification_group.reproducible:
             self.redo_modification_groups.append(
-                self._create_undo_modification_group(targeted_modification_group)
+                self._create_redo_modification_group(targeted_modification_group)
             )
 
             self.undo_depth += 1
@@ -177,7 +227,11 @@ class ModificationHandler:
 
         self._apply_modification_group(targeted_modification_group)
 
-    def redo(self):
+    def redo(self) -> None:
+        """
+        method to redo the latest not yet redone undone ModificationGroup (note that ModificationGroups that are
+        specified to not be redoable are skipped)
+        """
         if self.undo_depth == 0:
             return None
 
